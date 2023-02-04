39a835b37e71df9533cd7b78d243910c32942330
shuvo794 <shuvohossain794@gmail.com>
2023-02-04 09:20:05 +0600

    footer complete

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer
+      style={
+        {
+          // background: `url(${footer})`,
+          // backgroundSize: 'cover'
+        }
+      }
+      className="p-10 bg-neutral text-neutral-content"
+    >
+      <div className="footer ">
+        <div>
+          <span className="footer-title text-white">Services</span>
+          <Link to="/" className="link link-hover">
+            Medichine
+          </Link>
+          <Link to="/" className="link link-hover">
+            Liver
+          </Link>
+          <Link to="/" className="link link-hover">
+            Hart
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <Link to="/" className="link link-hover">
+            About us
+          </Link>
+          <Link to="/" className="link link-hover">
+            Contact
+          </Link>
+          <Link to="/" className="link link-hover">
+            Jobs
+          </Link>
+          <Link to="/" className="link link-hover">
+            Press kit
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link to="/" className="link link-hover">
+            Terms of use
+          </Link>
+          <Link to="/" className="link link-hover">
+            Privacy policy
+          </Link>
+          <Link to="/" className="link link-hover">
+            Cookie policy
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
