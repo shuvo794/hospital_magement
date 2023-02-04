d15300ed74885e4896062d5f1b3eb12ce70358db
shuvo794 <shuvohossain794@gmail.com>
2023-02-04 10:18:18 +0600

    service section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import About from "../../About/About";
+
 import Banner from "../Banner/Banner";
+import Services from "../../Services/Services";
 
 function Home() {
   return (
     <div>
       <Banner />
+      <Services />
       <About />
     </div>
   );
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,7 +1,46 @@
 import React from "react";
+import pic1 from "../../assets/1.png";
+import pic2 from "../../assets/2.png";
+import pic3 from "../../assets/3.png";
+import Service from "./Service";
 
-function Services() {
-  return <div>Services</div>;
-}
+const Services = () => {
+  const servicesData = [
+    {
+      id: 1,
+      name: "Medichine",
+      description:
+        "He is a provide good medichine all patient.and all patient happy her treatment",
+      img: pic1,
+    },
+    {
+      id: 2,
+      name: "Online Service",
+      description: "He is a provide a online service .",
+      img: pic2,
+    },
+    {
+      id: 3,
+      name: "Offline Service",
+      description: "He is a provide a offline service .",
+      img: pic3,
+    },
+  ];
+
+  return (
+    <div className="mt-16">
+      <div className="text-center">
+        <h3 className="text-xl font-bold text-primary uppercase">
+          Our Services
+        </h3>
+      </div>
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {servicesData.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Services;
