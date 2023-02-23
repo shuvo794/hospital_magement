9b9af91ba5c3463c9f20d77e8ff51a17ea8a3b27
shuvo794 <shuvohossain794@gmail.com>
2023-02-23 22:52:21 +0600

    work banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,12 @@
 import React from "react";
-import hospital from "../../../assets/hospital.jpg";
+
+import "./Bnner.css";
 
 function Banner() {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full banner">
       <div id="slide1" className=" relative w-full">
-        <img src={hospital} className="w-full" alt="" />
+        {/*  <img src={hospital} className="w-full" alt="" /> */}
       </div>
     </div>
   );
