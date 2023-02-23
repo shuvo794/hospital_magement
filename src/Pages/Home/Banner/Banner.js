93506898d88b70d7fd83b9d6b546bda0da78ecfe
shuvo794 <shuvohossain794@gmail.com>
2023-02-23 23:01:00 +0600

    fix-banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 import "./Bnner.css";
-
+import hospital from "../../../assets/doctor-3.jpg";
 function Banner() {
   return (
     <div className="carousel w-full banner">
       <div id="slide1" className=" relative w-full">
-        {/*  <img src={hospital} className="w-full" alt="" /> */}
+        <div className="fix-banner">
+          <div className="text-white">
+            <h2>WE WILL HELP YOU TO BECOME HEALTHY</h2>
+            <p className="text-white">
+              Feugiat primis ultrice in ligula risus auctor tempus feugiat
+              impedit Porta semper lacus cursus, feugiat primis ultrice in
+              ligula risus auctor tempus feugiat dolor impedit magna purus at
+              pretium donec
+            </p>
+          </div>
+          <div className="image-fix">
+            <img src={hospital} className="w-full" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
